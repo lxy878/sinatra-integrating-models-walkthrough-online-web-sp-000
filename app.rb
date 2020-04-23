16f67d9f34898023e9d5e17ca4242f31c5bc00b6
Xiaoyi Liu <lxy878@gmail.com>
2020-04-23 07:51:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-    puts text_from_user
+    puts text_from_user.class
     text_analyzer = TextAnalyzer.new(text_analyzer)
     puts text_analyzer.count_of_vowels
     erb :results
